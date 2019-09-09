ade0e73b5e5539f5ff9a43f9625bcbcfdc3cfccf
abharga <abharga@umich.edu>
2019-09-09 15:33:45 -0400

    Lab0

---
+++ Lab0.py
@@ -0,0 +1,8 @@
+from random import randint as rand
+print("Ankit Bhargava")
+alpha=rand(0,100)
+beta=rand(0,100)
+print(alpha)
+print(beta)
+print("Sum =",alpha+beta)
+print("Average =", (alpha+beta)/2)
